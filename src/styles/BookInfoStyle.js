5c7a21caadf46b829f830c07cbe7ad82bc2bf127
Salomon Romano <salo_romano@yahoo.com>
2022-02-28 10:18:28 -0600

    Agregar lista de Reviews al componente BookInfo

--- src/styles/BookInfoStyle.js
+++ src/styles/BookInfoStyle.js
@@ -4,15 +4,11 @@ import { StyleSheet } from 'react-native';
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    margin: 20,
   },
   header: {
     flexDirection: 'row',
     justifyContent: 'flex-start',
-    marginBottom: 30,
-  },
-  edges: {
-    padding: 10,
+    margin: 20,
   },
   title: {
     fontSize: 16,
@@ -25,5 +21,23 @@ export const styles = StyleSheet.create({
     width: 100,
     height: 100,
     marginRight: 10,
+  },
+  line: {
+    borderBottomColor: 'black',
+    borderBottomWidth: 1,
+  },
+  button: {
+    backgroundColor: '#ccc',
+    padding: 5,
+    borderRadius: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  plusIcon: {
+    marginRight: 5,
+  },
+  addReviewText: {
+    fontWeight: 'bold',
   },
 });
--- src/views/BookInfo.js
+++ src/views/BookInfo.js
@@ -1,42 +1,96 @@
 // Dependencies
-import React from 'react';
-import { View, Text, Image, TouchableOpacity } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, Image, TouchableOpacity, FlatList } from 'react-native';
+import { useIsFocused } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import axios from 'axios';
 
 // Constants
-import { IMG_URL } from '../constants';
+import { IMG_URL, API_URL } from '../constants';
 
 // Components
 import Star from 'components/Star';
+import Loading from 'components/Loading';
+import Review from 'components/Review';
 
 // Styles
 import { styles } from 'styles/BookInfoStyle';
 
 const BookInfo = ({ route, navigation }) => {
-  const { title, author, image, rating, id } = route.params.book;
+  const { title, author, image, id } = route.params.book;
+  const [newRating, setNewRating] = useState(0);
+  const [reviews, setReviews] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [refreshing, setRefreshing] = useState(false);
+
+  const isFocused = useIsFocused();
+
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get(`${API_URL}Books/${id}/reviews`)
+      .then(response => setReviews(response.data.sort((a, b) => b.id - a.id)))
+      .catch(err => console.log('My error', err));
+
+    axios
+      .get(`${API_URL}Books/${id}`)
+      .then(response => setNewRating(response.data.rating))
+      .catch(err => console.log('My error', err));
+    setLoading(false);
+  }, [id, isFocused]);
 
   return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <Image
-          source={{
-            uri: `${IMG_URL}${image}`,
-          }}
-          style={styles.image}
-        />
+    <Loading loading={loading}>
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Image
+            source={{
+              uri: `${IMG_URL}${image}`,
+            }}
+            style={styles.image}
+          />
 
-        <View style={styles.titleBook}>
-          <Text>{title}</Text>
-          <Text style={styles.author}>{author}</Text>
-          <Star rating={rating} />
-          <TouchableOpacity
-            onPress={() => {
-              navigation.navigate('reviews', { id });
-            }}>
-            <Text>Book Review</Text>
-          </TouchableOpacity>
+          <View style={styles.titleBook}>
+            <Text>{title}</Text>
+            <Text style={styles.author}>{author}</Text>
+            <Star rating={newRating} />
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => {
+                navigation.navigate('reviews', { id });
+              }}>
+              <Icon name="plus" style={styles.plusIcon} />
+              <Text style={styles.addReviewText}>Add Review</Text>
+            </TouchableOpacity>
+          </View>
         </View>
+        <FlatList
+          data={reviews}
+          renderItem={({ item, index }) => (
+            <Review review={item} index={index} />
+          )}
+          refreshing={refreshing}
+          onRefresh={() => {
+            setRefreshing(true);
+            setLoading(true);
+            axios
+              .get(`${API_URL}Books/${id}/reviews`)
+              .then(response =>
+                setReviews(response.data.sort((a, b) => b.id - a.id)),
+              )
+              .catch(err => console.log('My error', err));
+
+            axios
+              .get(`${API_URL}Books/${id}`)
+              .then(response => setNewRating(response.data.rating))
+              .catch(err => console.log('My error', err));
+            setLoading(false);
+            setRefreshing(false);
+          }}
+          keyExtractor={review => review.id}
+        />
       </View>
-    </View>
+    </Loading>
   );
 };
 
